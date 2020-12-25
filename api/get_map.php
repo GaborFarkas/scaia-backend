e0998d93e2cc9fbe51bc17a674fc37eedccf7bbf
GaborFarkas <raandal73@gmail.com>
2020-12-26 00:54:07 +0100

    Add basic map endpoint

---
+++ api/get_map.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '../admin/users/init.php';
+
+header('Content-Type: application/json;charset=utf-8');
+
+if (!$user->canServed()) {
+    http_response_code(401);
+    die();
+}
+if (!$user->isEligible()) {
+    http_response_code(403);
+    die('You are a teapot!');
+}
+
+
+?>
